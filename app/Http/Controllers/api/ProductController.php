70183615a47bd47595340ec21bcf785cb7087b15
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:16:15 +0800

    Created controller and request for Product
    Method:
    1. index

---
+++ app/Http/Controllers/api/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProductResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product = Product::latest()->get();
+
+        return ProductResource::collection($product);
+    }
+}
