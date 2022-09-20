50fe6f50c234b6beae0393c68d42ea2ebefead23
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:25:38 +0800

    Created seeders
    1. Database
    2. Order
    3. Product
    4. Transaction
    5. Wallet

---
+++ database/seeders/WalletSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Wallet;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WalletSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Wallet::factory()
+            ->count(11)
+            ->create();
+    }
+}
