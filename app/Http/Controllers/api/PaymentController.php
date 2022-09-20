4de5c97ec357406aa4f41fa58ab99584699f33c5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:15:03 +0800

    Created controller and view for Payment
    Methods:
    1. index
    2. runBill
    3. paymentConfirmation

---
+++ app/Http/Controllers/api/PaymentController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionStatus;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PaymentController extends Controller
+{
+    public function index($billId)
+    {
+        // Run Bill
+        $this->runBill($billId);
+    }
+
+    function runBill($billId)
+    {
+
+        $transaction = Transaction::findOrFail($billId);
+        $user = $transaction->user;
+
+        $some_data = array(
+            'userSecretKey' => config('payment-gateway.key'),
+            'billCode' => $billId,
+            'billpaymentPayorName' => $user->name,
+            'billpaymentPayorPhone' => $user->phone_number ?? '',
+            'billpaymentPayorEmail' => $user->email,
+        );
+
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_URL, config('payment-gateway.api') . 'runBill');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $some_data);
+
+        $result = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        $obj = json_decode($result);
+        echo $result;
+    }
+
+    public function paymentConfirmation()
+    {
+
+        // Get transaction model
+        $transaction = Transaction::findOrFail($billId);
+
+        // If status completed/successfull
+        if ($transaction->status == "Success") {
+            $title = __('Success');
+            $icon = __('✔');
+            $desc = __('Thank you for purchasing the diamonds.<br/>Your diamond balance will be updated shortly.<br/>You may close this page now.');
+            // Update user wallet ballance
+            Auth::user()->wallet()->increment('balance', $transaction->amount);
+        } else {
+            $title = __('Failed');
+            $desc = __('Failed to make the purchase, please try again later.<br/>You may close this page now.');
+            $icon = __('✘');
+        }
+
+        return view('paymentGatewayStatusPage', compact('title', 'desc', 'icon'));
+    }
+}
