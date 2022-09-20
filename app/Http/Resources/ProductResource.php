d949b5057d57d8757c4dd27adef9288ff13ae600
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:34:13 +0800

    Fix decimal places for Product (price)

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,11 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "image" => $this->image,
+            "price" => number_format($this->price, 2),
+            "title" => $this->title,
+        ];
     }
 }
