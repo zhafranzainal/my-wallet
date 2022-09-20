6b51944df2633944f6cc5f71488b2c448173257c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:26:16 +0800

    Configure API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\PaymentController;
+use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +18,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// To login
 Route::post('login', [AuthController::class, 'login']);
+Route::get('payment/{billId}', [PaymentController::class, 'index']);
+Route::get('payment-confirmation', [PaymentController::class, 'paymentConfirmation'])->name('paymentConfirmation');
 
 Route::middleware('auth:sanctum', 'verified')->group(function () {
 
     // To logout
     Route::get('logout', [AuthController::class, 'logout']);
+
+    // List products
+    Route::get('product', [ProductController::class, 'index']);
+
+    // Topup
+    Route::post('topup', [TransactionController::class, 'topup']);
+
+    // Checkout
+    Route::post('checkout', [TransactionController::class, 'checkout']);
+
 });
