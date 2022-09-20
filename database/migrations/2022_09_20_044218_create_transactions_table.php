c6ce945e033a6d6aea205c711488e323e3c0c8ed
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-09-21 01:10:55 +0800

    Created migrations
    1. Order
    2. Product
    3. Transaction
    4. Wallet

---
+++ database/migrations/2022_09_20_044218_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('id');
+            $table->foreignId('user_id');
+            $table->float('amount')->nullable();
+            $table->enum('status', ['Pending', 'Failed', 'Success'])->default('Pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
